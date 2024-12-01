f2e800e64273fe9757efd4d56c3be667979308fe
med-amine01 <m.chebbi.tech@gmail.com>
2024-12-01 11:19:06 +0100

    init property reservation project

---
+++ src/main/java/com/infor/propertyreservation/PropertyReservationApplication.java
@@ -0,0 +1,13 @@
+package com.infor.propertyreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infor/propertyreservation/PropertyReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infor.propertyreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
